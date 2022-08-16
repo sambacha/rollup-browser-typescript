f9fc3dbb68e53b5222fdcb4a5a84da44c9d85871
sam bacha <sam@manifoldfinance.com>
2022-08-15 21:42:21 -0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,42 @@
+
+
+/**
+ * @file Prettier configuration for Conformance
+ * @version 1.1.0
+ * @summary base config adapted from AirBNB to maximize performance
+ * @schema http://json.schemastore.org/prettierrc
+ */
+
+'use strict';
+
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'strict',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 110,
+  proseWrap: 'always',
+  quoteProps: 'consistent',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  importOrder: [
+    "^(react|react-dom)$",
+    "^next.+$",
+    "^@.+$",
+    "^[^\\/]+$",
+    "^.+\\.(svg|png|jpg|jpeg|webp)$",
+    "^.+\\/.+$",
+    "^[./]",
+    "^[../]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
